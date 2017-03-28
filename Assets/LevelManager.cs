387de8940b6aa1fb0758f7e3f671ec198c238401
Unknown <msf399@nyu.edu>
2017-03-28 12:16:26 -0400

    Adding Files to Gitkraken Properly

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public GameObject currentRespawn;
+
+	private WASD player;
+
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<WASD> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void RespawnPlayer() {
+
+		Debug.Log ("Player Respawn");
+		player.transform.position = currentRespawn.transform.position;
+
+	}
+}
